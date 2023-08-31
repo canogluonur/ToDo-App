571d6198a50130bcc0b2184c140046dcaae63d87
Onur <58385909+canogluonur@users.noreply.github.com>
2023-08-31 01:42:54 -0700

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,6 +6,6 @@ output "master_address" {
   value = "${aws_instance.k8s-master.public_dns}"
 }
 
-output "sock_shop_address" {
+output "todo-app-adress" {
   value = "${aws_elb.k8s-elb.dns_name}"
 }
